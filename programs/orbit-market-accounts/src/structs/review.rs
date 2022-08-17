d93246cf1559a1666fadede20fd4787970441444
indianeros58 <0xDEADBEAT@gmail.com>
2022-08-17 16:42:53 -0400

    review trait

--- programs/orbit-market-accounts/src/structs/review.rs
+++ programs/orbit-market-accounts/src/structs/review.rs
@@ -5,3 +5,9 @@ pub struct TransactionReviews{
     pub buyer: bool,
     pub seller: bool
 }
+
+#[error_code]
+pub enum ReviewErrors{
+    #[msg("reviews can only be from 1 to 5")]
+    RatingOutsideRange,
+}
