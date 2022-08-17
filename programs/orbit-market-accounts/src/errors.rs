caeac69b75b82c88f1fcea3b675dd79502d080fb
indianeros58 <0xDEADBEAT@gmail.com>
2022-08-17 17:38:12 -0400

    review trait

--- programs/orbit-market-accounts/src/errors.rs
+++ programs/orbit-market-accounts/src/errors.rs
@@ -3,5 +3,7 @@ use anchor_lang::prelude::*;
 #[error_code]
 pub enum MarketAccountErrors{
     #[msg("this pubkey is already used by a non-system program. please generate another one")]
-    InvalidMasterPubkey
+    InvalidMasterPubkey,
+    #[msg("Could not call orbit accounts program")]
+    CannotCallOrbitAccountsProgram,
 }
