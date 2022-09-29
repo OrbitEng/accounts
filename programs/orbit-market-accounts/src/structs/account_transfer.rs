2305a498f1ea3a68a9c886028f28bad3d688b7fc
indianeros58 <0xDEADBEAT@gmail.com>
2022-09-28 22:16:28 -0400

    trabsfer struct

--- programs/orbit-market-accounts/src/structs/account_transfer.rs
+++ programs/orbit-market-accounts/src/structs/account_transfer.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-#[accounts]
+#[account]
 pub struct AccountTransfer{
     pub source: Pubkey, // market account address of initiator
     pub destination: Pubkey, // market account address of recipient
